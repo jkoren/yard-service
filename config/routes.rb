1884547bf0d7c54c503cefd8d60d3565486025f3
jkoren <j.korenstein@gmail.com>
2021-01-25 17:21:25 -0500

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/addresses', to: 'addresses#index'
+  get '/streets', to: 'streets#index'
 end
